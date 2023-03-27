97640298c0a977c97f7493c8401ca9dab909b26e
DarshitAnadakat <110161814+DarshitAnadakat@users.noreply.github.com>
2023-03-27 19:42:31 +0530

    Add files via upload

---
+++ program18.py
@@ -0,0 +1,8 @@
+num_seq = input("Enter a sequence of comma-separated numbers: ")
+
+num_list = num_seq.split(",")
+
+num_tuple = tuple(num_list)
+
+print("List of numbers:", num_list)
+print("Tuple of numbers:", num_tuple)
